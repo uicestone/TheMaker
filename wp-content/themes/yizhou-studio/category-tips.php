5ccc83d9106ba8cc4fa080667c0d93c38fa67400
Uice Lu <uicestone@gmail.com>
2015-04-01 01:10:38 +0800

    修正: Tips页图片链接

--- wp-content/themes/yizhou-studio/category-tips.php
+++ wp-content/themes/yizhou-studio/category-tips.php
@@ -8,28 +8,28 @@
     <div class="col-lg-8 tips">
         <div class="row block-list paging-block">
             <div class="col-xs-6 col-sm-3">
-                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/6765.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/6765.jpg" alt="" width="136" height="136"></a>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <a href="http://www.nike.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/2.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.nike.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/2.jpg" alt="" width="136" height="136"></a>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/345.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/345.jpg" alt="" width="136" height="136"></a>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <a href="http://www.nike.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/nike.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.nike.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/nike.jpg" alt="" width="136" height="136"></a>
             </div>
             <div class="col-xs-6 col-sm-3">
-                <a href="http://www.chopard.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/78.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.chopard.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/78.jpg" alt="" width="136" height="136"></a>
             </div>
              <div class="col-xs-6 col-sm-3">
-                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/90.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/90.jpg" alt="" width="136" height="136"></a>
             </div>
              <div class="col-xs-6 col-sm-3">
-                <a href="http://www.chopard.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/89.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.chopard.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/89.jpg" alt="" width="136" height="136"></a>
             </div>
              <div class="col-xs-6 col-sm-3">
-                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/imagestips/5645.jpg" alt="" width="136" height="136"></a>
+                <a href="http://www.channel.com" title=""><img src="<?=get_stylesheet_directory_uri()?>/images/tips/5645.jpg" alt="" width="136" height="136"></a>
             </div>
         </div>
     </div>
