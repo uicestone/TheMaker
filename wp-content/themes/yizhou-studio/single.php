6d21306564eb4ed8237d509810364369d8887721
Uice Lu <uicestone@gmail.com>
2015-04-12 23:38:05 +0800

    fix #39

--- wp-content/themes/yizhou-studio/single.php
+++ wp-content/themes/yizhou-studio/single.php
@@ -3,8 +3,8 @@
    <?php the_content(); ?>
    <div class="visible-lg advertisement">
         <?php $ad_side = get_posts(array('tag'=>'content-ad-side', 'posts_per_page'=>2, 'orderby'=>'rand')); ?>
-        <?php if($ad_side){ ?>
-        <a href="<?=$ad_side[0]->post_content?>" target="_blank"><?=get_the_post_thumbnail($ad_side[0]->ID, 'ad-content-side')?></a>
+        <?php foreach($ad_side as $ad){ ?>
+        <a href="<?=$ad->post_content?>" target="_blank"><?=get_the_post_thumbnail($ad->ID, 'ad-content-side')?></a>
         <?php } ?>
    </div>
 </div>
