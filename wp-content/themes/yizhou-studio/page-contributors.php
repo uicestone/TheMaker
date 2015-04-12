16b4b91f27cbe510944a72f76371140f3d3b7674
Uice Lu <uicestone@gmail.com>
2015-04-12 23:11:27 +0800

    Contributors内容移至数据库 close #35

--- wp-content/themes/yizhou-studio/page-contributors.php
+++ wp-content/themes/yizhou-studio/page-contributors.php
@@ -1,70 +1,9 @@
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
 <div class="contributors">
-    <h1>The Maker Contributors</h1>
+    <h1><?php the_title(); ?></h1>
     <h6>Founder: YiZhou</h6>
-
-    <div class="">
-        <div class="row">
-            <div class="col-sm-4 col-md-3 founder">
-                <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/yizhou.jpg" alt="" width="315" height="420">
-            </div>
-            <div class="col-sm-8 col-md-9 team">
-                <div class="row category block-list international">
-                    <div class="col-xs-6 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Samantha-Culp2.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Samantha Culp</h6>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Aurelien-Heilbronn.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Aurelien Heilbronn</h6>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/David-Mushegain.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">David Mushegain</h6>
-                    </div>
-                    <div class="col-xs-6 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Salvo-De-Malebranche1.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Salvo De Malebranche</h6>
-                    </div>
-                </div>
-                <h5>The Maker Board</h5>
-                <div class="row category block-list china">
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Gaonan.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Gao Nan</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Amélie.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Kelvin Leung</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Callan.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Luna Zhang</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Ray.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Luna Zhang</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Deepak.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Deepak Kumar</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Sherman.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Sherman Wu</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Sasha.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Sasha Yu</h6>
-                    </div>
-                    <div class="col-xs-4 col-sm-3 category-item">
-                        <img src="<?=get_stylesheet_directory_uri()?>/images/contributors/Roy.jpg" width="135" height="135" alt="">
-                        <h6 class="contributor-name">Roy Zhao</h6>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+    <div class="row">
+        <?php the_content(); ?>
     </div>
 </div>
 <?php get_footer(); ?>
