b35f9d82dfd8c60d86661e5e4434809bab3881ff
Uice Lu <uicestone@gmail.com>
2015-04-14 16:24:11 +0800

    Contributors页面布局变动

--- wp-content/themes/yizhou-studio/page-contributors.php
+++ wp-content/themes/yizhou-studio/page-contributors.php
@@ -1,9 +1,6 @@
 <?php get_header(); the_post(); ?>
 <div class="contributors">
     <h1><?php the_title(); ?></h1>
-    <h6>Founder: YiZhou</h6>
-    <div class="row">
-        <?php the_content(); ?>
-    </div>
+    <?php the_content(); ?>
 </div>
 <?php get_footer(); ?>
